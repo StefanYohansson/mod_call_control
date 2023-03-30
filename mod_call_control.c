260db927323f75c2b62271dde6d0f1a93c95fe2d
StefanYohansson <stefan.yohansson@agnesit.tech>
2023-03-30 20:12:21 +0100

    add uuid_secret for handshake in API

--- mod_call_control.c
+++ mod_call_control.c
@@ -33,6 +33,7 @@
  */
 #include <switch.h>
 #include <switch_curl.h>
+#include "ks.h"
 
 #define MAX_FAIL_COUNT 10
 
@@ -45,6 +46,7 @@ SWITCH_MODULE_DEFINITION(mod_call_control, mod_call_control_load, mod_call_contr
 static struct {
 	char *webhook_allowed_events;
 	char *cc_api_host;
+	char *cc_api_external;
 	int cc_api_port;
 	switch_hash_t *tasks_hash;
 	switch_mutex_t *hash_mutex;
@@ -55,6 +57,7 @@ static switch_xml_config_int_options_t config_opt_cc_api_port = { SWITCH_TRUE, 1
 static switch_xml_config_item_t instructions[] = {
 	SWITCH_CONFIG_ITEM("webhook-allowed-events", SWITCH_CONFIG_STRING, CONFIG_RELOADABLE, &globals.webhook_allowed_events, "ALL", NULL, NULL, NULL),
 	SWITCH_CONFIG_ITEM("cc-api-host", SWITCH_CONFIG_STRING, CONFIG_RELOADABLE, &globals.cc_api_host, "localhost", NULL, NULL, NULL),
+	SWITCH_CONFIG_ITEM("cc-api-external", SWITCH_CONFIG_STRING, CONFIG_RELOADABLE, &globals.cc_api_external, "http://localhost:8055", NULL, NULL, NULL),
 	SWITCH_CONFIG_ITEM("cc-api-port", SWITCH_CONFIG_INT, CONFIG_RELOADABLE, &globals.cc_api_port, (void *) 8055, &config_opt_cc_api_port, NULL, NULL),
 	SWITCH_CONFIG_ITEM_END()
 };
@@ -62,6 +65,7 @@ static switch_xml_config_item_t instructions[] = {
 struct cc_task {
 	char *webhook_uri;
 	char *uuid;
+	char *uuid_secret;
 	switch_core_session_t *session;
 	switch_mutex_t *mutex;
 	switch_memory_pool_t *pool;
@@ -98,8 +102,9 @@ static void webhook_event_handler(switch_event_t *event)
 	char errbuf[CURL_ERROR_SIZE];
 	CURLcode res;
 	long rescode;
-	ks_json_t *json_res = ks_json_create_object();
-	ks_json_t *json_event = ks_json_create_object();
+	ks_json_t *json_res = NULL;
+	ks_json_t *json_event = NULL;
+	ks_pool_t *ks_pool = NULL;
 	char *json_str = NULL;
 	char *req = NULL;
 	struct response_data rd = { 0 };
@@ -144,14 +149,18 @@ static void webhook_event_handler(switch_event_t *event)
 		}
 
 		if (should_dispatch) {
+			ks_pool_open(&ks_pool);
+			json_event = ks_json_pcreate_object(ks_pool);
 			switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "[%s] [%s] Event can be dispatched\n", uuid, event_name);
+
 			if (switch_event_serialize_json_obj(event, &json_event) == SWITCH_STATUS_SUCCESS) {
-				ks_json_add_string_to_object(json_res, "uuid", uuid);
+				json_res = ks_json_pcreate_object(ks_pool);
+				ks_json_add_string_to_object(json_res, "api", globals.cc_api_external);
+				ks_json_add_string_to_object(json_res, "uuid", task->uuid);
+				ks_json_add_string_to_object(json_res, "uuid_secret", task->uuid_secret);
 				ks_json_add_item_to_object(json_res, "event", json_event);
 
-				json_str = ks_json_print_unformatted(json_res);
-				ks_json_delete(&json_event);
-				ks_json_delete(&json_res);
+				json_str = ks_json_pprint_unformatted(ks_pool, json_res);
 
 				curl = switch_curl_easy_init();
 
@@ -172,7 +181,7 @@ static void webhook_event_handler(switch_event_t *event)
 				switch_curl_easy_setopt(curl, CURLOPT_USERAGENT, "mod_call_control/1");
 				switch_curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_str);
 				switch_curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, errbuf);
-				switch_curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&rd);
+				//switch_curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&rd);
 
 				switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "[%s] [%s] Dispatching to webhook %s\n", uuid, event_name, task->webhook_uri);
 				switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "[%s] [%s] Dispatching json %s\n", uuid, event_name, json_str);
@@ -182,9 +191,10 @@ static void webhook_event_handler(switch_event_t *event)
 					switch_curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &rescode);
 
 					if (rescode != 200) {
-						switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Event delivery failed with HTTP code %ld, %s\n", rescode, rd.data);
+						//switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "[%s] [%s] Event delivery failed with HTTP code %ld, %s\n", uuid, event_name, rescode, rd.data);
 						task->fail_count = task->fail_count + 1;
 						if (task->fail_count >= MAX_FAIL_COUNT && task->running == 1) {
+							switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "[%s] [%s] Reached max retries to reach webhook %s, stopping task...\n", uuid, event_name, task->webhook_uri);
 							task->running = 0;
 						}
 					}
@@ -200,14 +210,11 @@ static void webhook_event_handler(switch_event_t *event)
 	switch_mutex_unlock(globals.hash_mutex);
 
  done:
-	switch_safe_free(rd.data);
-	switch_safe_free(json_str);
-	switch_safe_free(req);
+	if (ks_pool)
+		ks_pool_close(&ks_pool);
 
-	if (json_res)
-		ks_json_delete(&json_res);
-	if (json_event)
-		ks_json_delete(&json_event);
+	switch_safe_free(rd.data);
+	switch_safe_free(req);
 	return;
 }
 
@@ -224,6 +231,8 @@ SWITCH_STANDARD_API(call_control_function)
 		"--------------------------------------------------------------------------------\n"
 		"call_control start <uuid> <webhook>\n"
 		"call_control stop <uuid> <webhook>\n"
+		"call_control status\n"
+		"call_control list [debug]\n"
 		"--------------------------------------------------------------------------------\n";
 
 	if (zstr(cmd)) {
@@ -249,6 +258,7 @@ SWITCH_STANDARD_API(call_control_function)
 	if (!zstr(argv[0]) && !zstr(argv[1])) {
 		if (!strcasecmp(argv[0], "start")) {
 			switch_core_session_t *session = NULL;
+			ks_uuid_t uuid_secret;
 
 			if ((session = switch_core_session_locate(argv[1]))) {
 				switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "Found session to start call control on it\n");
@@ -261,12 +271,14 @@ SWITCH_STANDARD_API(call_control_function)
 				switch_mutex_unlock(globals.hash_mutex);
 
 				switch_core_new_memory_pool(&task_pool);
+				ks_uuid(&uuid_secret);
 
 				task = (cc_task_t *) switch_core_alloc(task_pool, sizeof(*task));
 				task->pool = task_pool;
 				switch_mutex_init(&task->mutex, SWITCH_MUTEX_NESTED, task->pool);
 				task->webhook_uri = NULL;
 				task->uuid = switch_core_strdup(task->pool, argv[1]);
+				task->uuid_secret = ks_uuid_str(NULL, &uuid_secret);
 				task->session = session;
 				task->running = 1;
 				task->fail_count = 0;
